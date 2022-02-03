369c7484b46488532a21b1e8ce3d48b8bf31aa5b
Chien <chien17099@gmail.com>
2022-02-04 02:44:09 +0700

    create new user

---
+++ EShopSolution.AdminApp/Controllers/LoginController.cs
@@ -0,0 +1,80 @@
+﻿using EShopSolution.AdminApp.Services;
+using EShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopSolution.AdminApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IUserApiClient _userApiClient;
+        private readonly IConfiguration _configuration;
+        public LoginController(IUserApiClient userApiClient, IConfiguration configuration)
+        {
+            _userApiClient = userApiClient;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View(ModelState);
+
+            var token = await _userApiClient.Authenticate(request);
+
+            var userPrincipal = this.ValidateToken(token);
+
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = false
+            };
+
+            HttpContext.Session.SetString("Token", token);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal, authProperties);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private ClaimsPrincipal ValidateToken(string jwtToken)
+        {
+            IdentityModelEventSource.ShowPII = true;
+
+            SecurityToken validatedToken;
+
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateLifetime = true,
+                ValidAudience = _configuration["Tokens:Issuer"],
+                ValidIssuer = _configuration["Tokens:Issuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]))
+            };
+
+            ClaimsPrincipal principal = new JwtSecurityTokenHandler().ValidateToken(jwtToken, validationParameters, out validatedToken);
+
+            return principal;
+        }
+    }
+}
